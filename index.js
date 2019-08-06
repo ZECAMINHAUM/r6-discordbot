98b81e6af65d7e8f3181564388625c35a84dc5de
sacuL174 <ls4388387@gmail.com>
2019-08-05 21:10:19 -0300

    setting bot profile image

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const antispam = require('discord-anti-spam'); // Requiring this module.
 const config = require('./config.json');
 const jimp = require('./src/jimp');
 const rainbowsix = require('./src/rainbowsix');
@@ -9,8 +10,25 @@ const fs = require('fs');
 const client =  new Discord.Client();
 
 client.on('ready', () => {
-    console.log(`Bot logado, tag: ${client.user.tag}`);
+
+    antispam(client, {
+        warnBuffer: 3, // Maximum ammount of messages allowed to send in the interval time before getting warned.
+        maxBuffer: 5, // Maximum amount of messages allowed to send in the interval time before getting banned.
+        interval: 2000, // Amount of time in ms users can send the maxim amount of messages(maxBuffer) before getting banned. 
+        warningMessage: "please stop spamming!", // Message users receive when warned. (message starts with '@User, ' so you only need to input continue of it.) 
+        banMessage: "has been hit by ban hammer for spamming!", // Message sent in chat when user is banned. (message starts with '@User, ' so you only need to input continue of it.) 
+        maxDuplicatesWarning: 7,// Maximum amount of duplicate messages a user can send in a timespan before getting warned.
+        maxDuplicatesBan: 10, // Maximum amount of duplicate messages a user can send in a timespan before getting banned.
+        deleteMessagesAfterBanForPastDays: 7, // Deletes the message history of the banned user in x days.
+        exemptRoles: ["Moderator"], // Name of roles (case sensitive) that are exempt from spam filter.
+        exemptUsers: ["MrAugu#9016"] // The Discord tags of the users (e.g: MrAugu#9016) (case sensitive) that are exempt from spam filter.
+      });
+
+
+    console.log(`Bot logado`);
+    client.user.setAvatar('./src/common/images/perfilbot.jpg');
     client.user.setActivity(`FLUXO`, { type: `VIAJA REGUEIRO`, url: `github.com/ZECAMINHAUM`}); 
+
 });
 
 client.on('guildCreate', guild => {
