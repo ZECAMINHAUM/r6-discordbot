86117b95fc869cfc01ab43a2a2983100e94efa0f
sacuL174 <ls4388387@gmail.com>
2019-09-23 13:59:45 -0300

    improve doc

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
-### DiscordBot for fun
+### Rainbow Six DiscordBot
 
-A bot for my clan discord server, only for fun.
+A Discord-Bot that i create for improve knowledge with nodejs and Automate searches in [R6 tab](https://r6tab.com/).
+
+---
+
+First Things First ( I probably say this in all my docs ), so lets begin!
+
+
+#### Instalation
+
+I'm Using yarn so if ur using too, just type in
+your console:
+
+```console
+$ yarn
+```
+
+But if you're using just npm type:
+
+```console
+$ npm install
+```
+
+#### Playing
+
+Now you can enjoy, VIAJA REGUEIRO! the first version has only the command for start, but I will improve soon! Just run:
+
+```console
+$ yarn start
+```
+or
+```console
+$ npm start
+```
+
+#### What I use
+
+    * Node.js
+    * Discord.js
+    * Axios
+    * Jimp
+    * rainbowsix-siege-api
+
+
--- index.js
+++ index.js
@@ -7,27 +7,13 @@ const fs = require('fs');
 
 
 
-const client =  new Discord.Client();
+const client = new Discord.Client();
 
 client.on('ready', () => {
 
-    antispam(client, {
-        warnBuffer: 3, // Maximum ammount of messages allowed to send in the interval time before getting warned.
-        maxBuffer: 5, // Maximum amount of messages allowed to send in the interval time before getting banned.
-        interval: 2000, // Amount of time in ms users can send the maxim amount of messages(maxBuffer) before getting banned. 
-        warningMessage: "please stop spamming!", // Message users receive when warned. (message starts with '@User, ' so you only need to input continue of it.) 
-        banMessage: "has been hit by ban hammer for spamming!", // Message sent in chat when user is banned. (message starts with '@User, ' so you only need to input continue of it.) 
-        maxDuplicatesWarning: 7,// Maximum amount of duplicate messages a user can send in a timespan before getting warned.
-        maxDuplicatesBan: 10, // Maximum amount of duplicate messages a user can send in a timespan before getting banned.
-        deleteMessagesAfterBanForPastDays: 7, // Deletes the message history of the banned user in x days.
-        exemptRoles: ["Moderator"], // Name of roles (case sensitive) that are exempt from spam filter.
-        exemptUsers: ["MrAugu#9016"] // The Discord tags of the users (e.g: MrAugu#9016) (case sensitive) that are exempt from spam filter.
-      });
-
-
     console.log(`Bot logado`);
     client.user.setAvatar('./src/common/images/perfilbot.jpg');
-    client.user.setActivity(`FLUXO`, { type: `VIAJA REGUEIRO`, url: `github.com/ZECAMINHAUM`}); 
+    client.user.setActivity(`FLUXO`, { type: `VIAJA REGUEIRO`, url: `github.com/ZECAMINHAUM` });
 
 });
 
@@ -45,31 +31,31 @@ client.on('guildMemberAvailable', guild => {
 
 });
 client.on('message', async msg => {
-    if(msg.author.bot) return;
-    
+    if (msg.author.bot) return;
+
     const args = msg.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
     console.log(args);
-    if(command === 'ping'){
+    if (command === 'ping') {
         const m = await msg.channel.send('Ping?');
         msg.reply(`PONG! A latencia é ${m.createdTimestamp - msg.createdTimestamp}ms.
                 \n A Latencia do Bot é de ${Math.round(client.ping)}ms`);
     }
-    if(command === 'teste'){
-        const json = { 
+    if (command === 'teste') {
+        const json = {
             "teste": "teste"
         }
         console.log(json);
         msg.channel.send(json);
     }
-    if(command === 'chama'){
+    if (command === 'chama') {
         const username = args[0];
         rainbowsix.getUserByName(username, msg);
 
     }
-    if(command === 'fluxo'){
+    if (command === 'fluxo') {
         jimp.hello(msg.author);
-        msg.reply(`VIAJA REGUEIRO`, {files: [`./src/user.png`]});
+        msg.reply(`VIAJA REGUEIRO`, { files: [`./src/user.png`] });
     }
 });
 
